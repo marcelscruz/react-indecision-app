e887413e991224dd9167375abbc2af834b5cc9d6
Marcel Cruz <marcelcruz@live.com>
2018-02-17 23:35:50 +0000

    counter added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,94 @@
 'use strict';
 
-// const square = function (x) {
-//   return x * x;
-// };
-//
-// const squareArrow = (x) => {
-//   return x * x;
-// };
-//
-// const squareArrow2 = (x) => x * x;
-//
-// console.log(square(8));
-// console.log(squareArrow(9));
-
-// Challenge - use arrow functions
-// Create regular arrow function
-// Create arrow function using shorthand syntax
-
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
+// create app object title/subtitle
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Let a machine decide for you',
+  options: ['One', 'Two']
 };
 
-var getFirstName2 = function getFirstName2(fullName) {
-  return fullName.split(' ')[0];
+// JSX - JavaScript XML
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options: ' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var count = 0;
+
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
 };
 
-console.log(getFirstName('Marcel Cruz'));
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+};
 
-console.log(getFirstName2('First Last'));
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+};
+
+var appRoot = document.getElementById('app');
+
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -3,35 +3,46 @@
 const add = (a, b) => {
   // console.log(arguments);
   return a + b;
-};
+}
 
-console.log(add(55, 1));
+console.log(add(55, 11));
 
 // this keyword - no longer bound
 
 const user = {
   name: 'Marcel',
-  cities: ['Dublin', 'Pelotas'],
+  cities: ['Pelotas', 'Dublin'],
+  // printPlacesLived: function () {
+  // new es6 method syntax
   printPlacesLived() {
-    return this.cities.map((city) => this.name + ' has lived in ' + city);
+    // map allows to create a new array
+    return this.cities.map((city) => this.name + ' has lived in ' + city + '!');
 
     // console.log(this.name);
     // console.log(this.cities);
+
+    // won't work, this.name is in a higher scope
+    // this.cities.forEach(function (city) {
+    //   console.log(this.name + ' has lived in ' + city);
+    // });
 
     // this.cities.forEach((city) => {
     //   console.log(this.name + ' has lived in ' + city);
     // });
   }
-};
+}
 
 console.log(user.printPlacesLived());
 
 const multiplier = {
-  numbers: [3, 6],
+  // number - array of numbers
+  numbers: [2, 3, 4],
   multiplyBy: 2,
   multiply() {
     return this.numbers.map((number) => number * this.multiplyBy);
   }
+  // multiplyBy - single number
+  // multiply - return a new array where the number have been multiplied
 };
 
 console.log(multiplier.multiply());
