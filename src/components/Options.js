bb429967d44ee8aa60c39079b78c4fbb87153f47
Marcel Cruz <marcelcruz@live.com>
2018-02-21 19:41:16 +0000

    style added to buttons

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,15 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove all</button>
+    <div className='widget-header'>
+      <h3>Your options</h3>
+      <button
+        className='button button--link'
+        onClick={props.handleDeleteOptions}
+      >
+        Remove all
+      </button>
+    </div>
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
     {
       props.options.map((option) =>
